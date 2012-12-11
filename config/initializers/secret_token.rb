949061eadf951cc0d428c0bbed951e72bd2330ce
Larry Buchanan <larrybuch@gmail.com>
2012-12-11 14:17:31 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubwayAlert::Application.config.secret_token = '89b2042fa9b1ae61865ba2c38df47f91f0ed4db6c7967b04b826db16560f418575a26db13ea9ca75173edae1d9bbf6b6778f358baf3a07160de4faf735b41d13'
