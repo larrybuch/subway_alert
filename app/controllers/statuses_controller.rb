760e5d007643582d895e31bdadbd0bd932f7d804
Larry Buchanan <larrybuch@gmail.com>
2012-12-14 15:45:54 -0500

    added specific message to the DELAYS service

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,6 @@
 class StatusesController < ApplicationController
   def create
-    x = HTTParty.get('http://larrybuch.com/mta.txt')
+    x = HTTParty.get('http://www.mta.info/status/serviceStatus.txt')
     y = Hash.from_xml(x)
     lines = y["service"]["subway"]["line"]
     count = lines.size
@@ -16,18 +16,19 @@ class StatusesController < ApplicationController
         users_to_text = line.users #returns an array of users who are tracking that line
         number_to_text = users_to_text.size #returns the number of users in the array to text
 
-        if line.statuses[0].condition = "DELAYS"
+        if line.statuses[0].condition == "DELAYS"
           number_to_text.times do |num|
             phone_number = users_to_text[num].phone
             client = Twilio::REST::Client.new(TW_SID, TW_TOK)
-            @message = client.account.sms.messages.create({:from => '+19177463330', :to => phone_number, :body => 'Aw Snap! The trains down'})
+            status_update = line.statuses[0].info.split(/\n/)[5].strip.gsub('<STRONG>', '').gsub('</STRONG>', '') #parses the MTA data to give only the status update.
+            @message = client.account.sms.messages.create({:from => '+19177463330', :to => phone_number, :body => "Aw Snap! #{status_update}"})
           end
 
-        elsif line.statuses[0].condition = "GOOD SERVICE"
+        elsif line.statuses[0].condition == "GOOD SERVICE"
           number_to_text.times do |num|
             phone_number = users_to_text[num].phone
             client = Twilio::REST::Client.new(TW_SID, TW_TOK)
-            @message = client.account.sms.messages.create({:from => '+19177463330', :to => phone_number, :body => 'Woo woo! The trains back up.'})
+            @message = client.account.sms.messages.create({:from => '+19177463330', :to => phone_number, :body => "Woo woo! The #{line.line} is back up."})
           end
         end
       end
@@ -36,3 +37,7 @@ class StatusesController < ApplicationController
     redirect_to root_path
   end
 end
+
+
+
+#.statuses[1].info.split(/\n/)[5]
