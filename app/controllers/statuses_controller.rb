686c8be1615ad803c75e901f06c335e9cf3b2148
Larry Buchanan <larrybuch@gmail.com>
2012-12-14 11:24:24 -0500

    updating status's when you click the get data button

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,15 +1,24 @@
 class StatusesController < ApplicationController
   def create
-    x = HTTParty.get('http://www.mta.info/status/serviceStatus.txt')
+    x = HTTParty.get('http://larrybuch.com/mta.txt')
     y = Hash.from_xml(x)
     lines = y["service"]["subway"]["line"]
     count = lines.size
 
     count.times do |num|
-      line    = Line.find(num + 1)
-      status  = lines[num]["status"]
-      info    = lines[num]["text"]
-      line.statuses.create(:condition => status, :info => info)
+      line       = Line.find(num + 1)
+      condition  = lines[num]["status"]
+      info       = lines[num]["text"]
+      if condition != line.statuses[0].condition
+        line.statuses[0].update_attributes(:condition => condition, :info => info)
+        if line.statuses[0].condition = "DELAYS"
+          puts "Delays!"
+        elsif line.statuses[0].condition = "GOOD SERVICE"
+          puts "Good Service!"
+        end
+      end
     end
+
+    redirect_to root_path
   end
 end
