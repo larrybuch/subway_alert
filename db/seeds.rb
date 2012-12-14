3b9f62403665903d2d3eda482e49c4dc97c35672
Larry Buchanan <larrybuch@gmail.com>
2012-12-14 17:55:41 -0500

    added lines to seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
 User.delete_all
+
+Line.create(:line => '123')
+Line.create(:line => '456')
+Line.create(:line => '7')
+Line.create(:line => 'ACE')
+Line.create(:line => 'BDFM')
+Line.create(:line => 'G')
+Line.create(:line => 'JZ')
+Line.create(:line => 'L')
+Line.create(:line => 'NQR')
+Line.create(:line => 'S')
+Line.create(:line => 'SIR')
