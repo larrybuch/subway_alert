029e76aa443e872bf3e6df90e2a248c8dde254ef
Larry Buchanan <larrybuch@gmail.com>
2012-12-17 09:33:18 -0500

    modified helper to show login/logout based on @authenticated user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def show_login_or_logout_link
+    if @authenticated_user
+      link_to "Logout", logout_path
+    else
+      link_to "Login", login_path
+    end
+  end
+
+  def show_user_name
+    if @authenticated_user
+      @authenticated_user.name
+    end
+  end
 end
