422f9e0c7cca5c6fc629986c142daccc1f91dfab
Larry Buchanan <larrybuch@gmail.com>
2012-12-18 15:03:39 -0500

    now users can't edit everyone else

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,9 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     @lines = Line.all
+    if @authenticated_user != @user
+      redirect_to root_path
+    end
   end
 
   def update
